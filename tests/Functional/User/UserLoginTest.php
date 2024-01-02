e0ae9ef3c022c85b4829f72466f2994b785ffb36
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-02 15:42:16 +0100

    added test

---
+++ tests/Functional/User/UserLoginTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Tests\Functional\User;
+
+use App\Factory\UserFactory;
+use App\Tests\Functional\Helper\ApiTestCase;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class UserLoginTest extends ApiTestCase
+{
+    use ResetDatabase;
+
+    /** @test */
+    public function jwt_token_generation()
+    {
+        $user = UserFactory::createOne();
+
+        $this->browser()
+            ->post('/api/login_check', [
+                'json' => [
+                    'email' => $user->getEmail(),
+                    'password' => 'password',
+                ],
+            ])
+            ->assertStatus(200)
+            ->assertContains('token');
+    }
+
+    /** @test */
+    public function user_login_with_jwt_token()
+    {
+        $user = UserFactory::createOne();
+
+        $response = $this->browser()
+            ->post('/api/login_check', [
+                'json' => [
+                    'email' => $user->getEmail(),
+                    'password' => 'password',
+                ]
+            ]);
+
+        $jwtToken = json_decode($response->json(), true)['token'];
+
+        $this->browser()
+            ->get('/api/users', [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $jwtToken,
+                ],
+            ])
+            ->assertAuthenticated();
+    }
+}
