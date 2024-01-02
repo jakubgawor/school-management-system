6dca3402af36014072522907266aa4d9d1686a83
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-02 21:09:52 +0100

    added UniqueEmailValidator

---
+++ src/Validator/UniqueEmailValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Validator;
+
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class UniqueEmailValidator extends ConstraintValidator
+{
+    public function __construct(
+        private UserRepository $userRepository,
+    )
+    {
+    }
+
+    public function validate($value, Constraint $constraint)
+    {
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if ($this->userRepository->findOneBy(['email' => $value])) {
+            $this->context->buildViolation($constraint->message)
+                ->addViolation();
+        }
+    }
+}
