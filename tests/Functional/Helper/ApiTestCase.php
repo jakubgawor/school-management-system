7729b41a97afead8df2de7187db9dc312d3e88d8
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-02 20:59:49 +0100

    refactoring

--- tests/Functional/Helper/ApiTestCase.php
+++ tests/Functional/Helper/ApiTestCase.php
@@ -3,11 +3,23 @@
 namespace App\Tests\Functional\Helper;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Browser\HttpOptions;
+use Zenstruck\Browser\KernelBrowser;
 use Zenstruck\Browser\Test\HasBrowser;
-use Zenstruck\Foundry\Test\ResetDatabase;
 
 abstract class ApiTestCase extends KernelTestCase
 {
-    use HasBrowser;
-    use ResetDatabase;
+    use HasBrowser {
+        browser as baseKernelBrowser;
+    }
+
+    protected function browser(array $options = [], array $server = []): KernelBrowser
+    {
+        return $this->baseKernelBrowser($options, $server)
+            ->setDefaultHttpOptions(
+                HttpOptions::create()
+                ->withHeader('Accept', 'application/ld+json')
+            );
+    }
+
 }
