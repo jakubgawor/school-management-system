c92ad131abeb189c2c2644f35bcc04f0f2b48138
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-02 21:00:51 +0100

    updated UserApi

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -11,20 +11,32 @@ use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use App\Entity\User;
+use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ApiResource(
     shortName: 'User',
     operations: [
         new Get(),
         new GetCollection(),
-        new Post(),
-        new Patch(),
-        new Delete(),
+        new Post(
+            security: 'is_granted("PUBLIC_ACCESS")',
+            validationContext: ['groups' => ['Default', 'postValidation']]
+        ),
+        new Patch(
+            security: 'is_granted("PATCH", object)'
+        ),
+        new Delete(
+            security: 'is_granted("DELETE", object)'
+        ),
     ],
     paginationItemsPerPage: 10,
+    security: 'is_granted("ROLE_USER")',
     provider: EntityToDtoStateProvider::class,
+    processor: EntityClassDtoStateProcessor::class,
     stateOptions: new Options(User::class),
 )]
 class UserApi
@@ -32,5 +44,11 @@ class UserApi
     #[ApiProperty(readable: false, writable: false, identifier: true)]
     public ?Uuid $id = null;
 
+    #[NotBlank]
+    #[Email]
     public ?string $email = null;
+
+    #[NotBlank(groups: ['postValidation'])]
+    #[ApiProperty(readable: false)]
+    public ?string $password = null;
 }
