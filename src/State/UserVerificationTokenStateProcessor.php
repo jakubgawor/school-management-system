3089e1ab4c87255a6e47a0b9b4171d2ddb3036f2
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-03 15:32:11 +0100

    created UserVerificationTokenStateProcessor.php

---
+++ src/State/UserVerificationTokenStateProcessor.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\ApiResource\UserVerificationTokenApi;
+use App\Service\VerificationUserMailService;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+class UserVerificationTokenStateProcessor implements ProcessorInterface
+{
+    public function __construct(
+        #[Autowire(service: PersistProcessor::class)] private ProcessorInterface $processor,
+        private VerificationUserMailService                                      $verificationUserMailService,
+    )
+    {
+
+    }
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        assert($data instanceof UserVerificationTokenApi);
+
+        $this->verificationUserMailService->verifyUser($data->getToken());
+
+        $this->processor->process($data, $operation, $uriVariables, $context);
+
+
+        return $data;
+    }
+}
