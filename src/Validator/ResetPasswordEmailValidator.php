3fbad4860116f4008c925efb3cd1d92104529563
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-06 03:54:19 +0100

    added validator

---
+++ src/Validator/ResetPasswordEmailValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Validator;
+
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class ResetPasswordEmailValidator extends ConstraintValidator
+{
+    public function __construct(
+        private UserRepository $userRepository,
+    )
+    {
+    }
+
+    public function validate($value, Constraint $constraint)
+    {
+        $user = $this->userRepository->findOneBy(['email' => $value]);
+
+        if(!$user) {
+            $this->context->buildViolation($constraint->notExistingUser)
+                ->addViolation();
+        }
+
+    }
+}
