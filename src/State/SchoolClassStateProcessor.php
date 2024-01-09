5ff646b9b9a0b85277c5f82a993bc53a407a92f0
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-09 18:52:53 +0100

    added SchoolClassStateProcessor

---
+++ src/State/SchoolClassStateProcessor.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\SchoolClass;
+use App\Service\SchoolClassService;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+class SchoolClassStateProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private EntityClassDtoStateProcessor $innerProcessor,
+        private MicroMapperInterface         $microMapper,
+        private SchoolClassService           $schoolClassService,
+    )
+    {
+    }
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        if ($operation instanceof Delete) {
+            $this->schoolClassService->removeStudentsFromClass($this->microMapper->map(
+                $data,
+                SchoolClass::class
+            ));
+        }
+
+        return $this->innerProcessor->process($data, $operation, $uriVariables, $context);
+    }
+}
