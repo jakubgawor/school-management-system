8a2c7d5b6861d4d9b944f150999cc6ff0143d51b
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-11 16:46:21 +0100

    added dto

---
+++ src/Dto/TeacherNameDto.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Dto;
+
+use ApiPlatform\Metadata\ApiProperty;
+
+class TeacherNameDto
+{
+    #[ApiProperty(readable: false, writable: false, identifier: true)]
+    private ?int $id = null;
+
+    public ?string $firstName = null;
+    public ?string $lastName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+}
