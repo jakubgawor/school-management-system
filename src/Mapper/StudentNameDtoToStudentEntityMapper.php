d5d58b4bb773f360f08b0457724013fef262c869
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-11 16:46:27 +0100

    added mapper

---
+++ src/Mapper/StudentNameDtoToStudentEntityMapper.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mapper;
+
+use App\Dto\StudentNameDto;
+use App\Entity\Student;
+use App\Repository\StudentRepository;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+
+#[AsMapper(from: StudentNameDto::class, to: Student::class)]
+class StudentNameDtoToStudentEntityMapper implements MapperInterface
+{
+    public function __construct(
+        private StudentRepository $studentRepository,
+    )
+    {
+    }
+
+    public function load(object $from, string $toClass, array $context): object
+    {
+        $dto = $from;
+        assert($dto instanceof StudentNameDto);
+
+        $studentEntity = $dto->getId() ? $this->studentRepository->find($dto->getId()) : new Student();
+
+        if (!$studentEntity) {
+            throw new \Exception('Student not found.');
+        }
+
+        return $studentEntity;
+    }
+
+    public function populate(object $from, object $to, array $context): object
+    {
+        $dto = $from;
+        $entity = $to;
+        assert($dto instanceof StudentNameDto);
+        assert($entity instanceof Student);
+
+        $entity->setFirstName($dto->firstName);
+        $entity->setLastName($dto->lastName);
+
+        return $entity;
+    }
+}
---
+++ src/Mapper/TeacherNameDtoToStudentEntityMapper.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mapper;
+
+use App\Dto\TeacherNameDto;
+use App\Entity\Teacher;
+use App\Repository\TeacherRepository;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+
+#[AsMapper(from: TeacherNameDto::class, to: Teacher::class)]
+class TeacherNameDtoToStudentEntityMapper implements MapperInterface
+{
+    public function __construct(
+        private TeacherRepository $teacherRepository,
+    )
+    {
+    }
+
+    public function load(object $from, string $toClass, array $context): object
+    {
+        $dto = $from;
+        assert($dto instanceof TeacherNameDto);
+
+        $teacherEntity = $dto->getId() ? $this->teacherRepository->find($dto->getId()) : new Teacher();
+
+        if (!$teacherEntity) {
+            throw new \Exception('Teacher not found.');
+        }
+
+        return $teacherEntity;
+    }
+
+    public function populate(object $from, object $to, array $context): object
+    {
+        $dto = $from;
+        $entity = $to;
+        assert($dto instanceof TeacherNameDto);
+        assert($entity instanceof Teacher);
+
+        $entity->setFirstName($dto->firstName);
+        $entity->setLastName($dto->lastName);
+
+        return $entity;
+    }
+}
