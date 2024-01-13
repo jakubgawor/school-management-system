ecd733e68627e763b460375336f610896a2fd886
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-13 22:46:33 +0100

    added SchoolClassStudentManagementStateProvider

---
+++ src/State/SchoolClassStudentManagementStateProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\Service\StudentService;
+
+class SchoolClassStudentManagementStateProvider implements ProviderInterface
+{
+    public function __construct(
+        private StudentService $studentService,
+    )
+    {
+    }
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        return $this->studentService->getStudentById($uriVariables['studentId']);
+    }
+}
