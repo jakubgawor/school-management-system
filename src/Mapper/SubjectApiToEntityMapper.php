1b1a38941d97e5867918d39d8b6f1957d9d789f0
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-16 17:56:29 +0100

    added mappers

---
+++ src/Mapper/SubjectApiToEntityMapper.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mapper;
+
+use App\ApiResource\SubjectApi;
+use App\Entity\Subject;
+use App\Entity\Teacher;
+use App\Repository\SchoolClassRepository;
+use App\Repository\SubjectRepository;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+#[AsMapper(from: SubjectApi::class, to: Subject::class)]
+class SubjectApiToEntityMapper implements MapperInterface
+{
+    public function __construct(
+        private SubjectRepository         $subjectRepository,
+        private MicroMapperInterface      $microMapper,
+        private SchoolClassRepository     $schoolClassRepository,
+
+    )
+    {
+    }
+
+    public function load(object $from, string $toClass, array $context): object
+    {
+        $dto = $from;
+        assert($dto instanceof SubjectApi);
+
+        $subjectEntity = $dto->getId() ? $this->subjectRepository->find($dto->getId()) : new Subject();
+
+        if (!$subjectEntity) {
+            throw new \Exception('Subject not found.');
+        }
+
+        return $subjectEntity;
+    }
+
+    public function populate(object $from, object $to, array $context): object
+    {
+        $dto = $from;
+        $entity = $to;
+        assert($dto instanceof SubjectApi);
+        assert($entity instanceof Subject);
+
+        $entity->setName($dto->getName());
+
+        foreach ($dto->getSchoolClasses() as $schoolClassName) {
+            $schoolClass = $this->schoolClassRepository->findOneBy(['name' => $schoolClassName]);
+            $entity->addSchoolClass($schoolClass);
+        }
+
+        $entity->setTeacher($this->microMapper->map($dto->getTeacher(), Teacher::class, [
+            MicroMapperInterface::MAX_DEPTH => 0,
+        ]));
+
+        return $entity;
+    }
+}
---
+++ src/Mapper/SubjectEntityToApiMapper.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mapper;
+
+use App\ApiResource\SchoolClassApi;
+use App\ApiResource\SubjectApi;
+use App\ApiResource\TeacherApi;
+use App\Entity\SchoolClass;
+use App\Entity\Subject;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MapperInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+#[AsMapper(from: Subject::class, to: SubjectApi::class)]
+class SubjectEntityToApiMapper implements MapperInterface
+{
+    public function __construct(
+        private MicroMapperInterface $microMapper,
+    )
+    {
+    }
+
+    public function load(object $from, string $toClass, array $context): object
+    {
+        $entity = $from;
+        assert($entity instanceof Subject);
+
+        $dto = new SubjectApi();
+        $dto->setId($entity->getId());
+
+        return $dto;
+    }
+
+    public function populate(object $from, object $to, array $context): object
+    {
+        $entity = $from;
+        $dto = $to;
+        assert($entity instanceof Subject);
+        assert($dto instanceof SubjectApi);
+
+        $dto->setName($entity->getName());
+
+        $dto->setTeacher($this->microMapper->map($entity->getTeacher(), TeacherApi::class, [
+            MicroMapperInterface::MAX_DEPTH => 1,
+        ]));
+
+        $dto->setSchoolClasses(array_map(function (SchoolClass $schoolClass) {
+            return $this->microMapper->map($schoolClass, SchoolClassApi::class, [
+                MicroMapperInterface::MAX_DEPTH => 1,
+            ])->getName();
+        }, $entity->getSchoolClasses()->getValues()));
+
+
+        return $dto;
+    }
+}
