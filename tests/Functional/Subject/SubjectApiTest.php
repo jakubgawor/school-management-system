203f072f021f06de4d5dd1a88a8ad441126674e0
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-16 17:56:59 +0100

    added tests

---
+++ tests/Functional/Subject/SubjectApiTest.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Tests\Functional\Subject;
+
+use App\Factory\SubjectFactory;
+use App\Factory\TeacherFactory;
+use App\Tests\Functional\Helper\ApiTestCase;
+use Zenstruck\Browser\Json;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class SubjectApiTest extends ApiTestCase
+{
+    use ResetDatabase;
+    use Factories;
+
+    /** @test */
+    public function get_collection_of_subjects()
+    {
+        SubjectFactory::createOne();
+
+        $this->browser()
+            ->get('/api/subjects')
+            ->assertStatus(200)
+            ->use(function (Json $json) {
+                $json->assertMatches('keys("hydra:member"[0])', [
+                    '@id',
+                    '@type',
+                    'name',
+                    'schoolClasses',
+                    'teacher',
+                ]);
+            });
+    }
+
+    /** @test */
+    public function trying_to_retrieve_not_existing_subject()
+    {
+        $this->browser()
+            ->get('/api/subjects/40')
+            ->assertStatus(404);
+    }
+
+    /** @test */
+    public function post_to_create_subject()
+    {
+        $teacher = TeacherFactory::createOne();
+
+        $this->browser()
+            ->post('/api/subjects', [
+                'json' => [
+                    'name' => 'biology',
+                    'teacher' => '/api/teachers/' . $teacher->getId()
+                ]
+            ])->assertStatus(201);
+    }
+
+    /** @test */
+    public function post_to_create_subject_without_teacher()
+    {
+        $this->browser()
+            ->post('/api/subjects', [
+                'json' => [
+                    'name' => 'biology',
+                ]
+            ])->assertStatus(422);
+    }
+
+    /** @test */
+    public function post_to_create_subject_without_name()
+    {
+        $this->browser()
+            ->post('/api/subjects', [
+                'json' => [
+                    'teacher' => '/api/teachers/' . TeacherFactory::createOne()->getId(),
+                ]
+            ])->assertStatus(422);
+    }
+
+    /** @test */
+    public function post_to_create_new_subject_with_not_existing_teacher()
+    {
+        $this->browser()
+            ->post('/api/subjects', [
+                'json' => [
+                    'name' => '',
+                    'teacher' => '/api/teachers/43',
+                ]
+            ])->assertStatus(404);
+    }
+
+    /** @test */
+    public function post_to_create_new_subject_same_name_provided()
+    {
+        $teacher = TeacherFactory::createOne();
+        $subject = SubjectFactory::createOne(['teacher' => $teacher]);
+
+        $this->browser()
+            ->post('/api/subjects', [
+                'json' => [
+                    'name' => $subject->getName(),
+                    'teacher' => '/api/teachers/' . $teacher->getId(),
+                ]
+            ])->assertStatus(422);
+
+    }
+
+    /** @test */
+    public function post_to_create_subject_with_existing_name_belongs_to_other_teacher()
+    {
+        SubjectFactory::createOne(['name' => 'biology', 'teacher' => TeacherFactory::createOne()]);
+        $teacher = TeacherFactory::createOne();
+
+        $this->browser()
+            ->post('/api/subjects', [
+                'json' => [
+                    'name' => 'biology',
+                    'teacher' => '/api/teachers/' . $teacher->getId(),
+                ]
+            ])->assertStatus(201);
+    }
+
+    /** @test */
+    public function delete_subject()
+    {
+        $subject = SubjectFactory::createOne();
+
+        $this->browser()
+            ->delete('/api/subjects/' . $subject->getId())
+            ->assertStatus(204);
+    }
+
+    /** @test */
+    public function patch_to_update_subject()
+    {
+        $subject = SubjectFactory::createOne();
+        $teacher = TeacherFactory::createOne();
+
+        $this->browser()
+            ->patch('/api/subjects/' . $subject->getId(), [
+                'json' => [
+                    'name' => 'chemistry',
+                    'teacher' => '/api/teachers/' . $teacher->getId(),
+                ],
+                'headers' => ['Content-Type' => 'application/merge-patch+json']
+            ])
+            ->assertStatus(200);
+
+        $this->assertSame($subject->getTeacher()->getId(), $teacher->getId());
+    }
+
+    /** @test */
+    public function patch_to_update_subject_same_name_provided()
+    {
+        $teacher = TeacherFactory::createOne();
+        $subject1 = SubjectFactory::createOne(['teacher' => $teacher]);
+        $subject2 = SubjectFactory::createOne(['teacher' => $teacher]);
+
+        $this->browser()
+            ->patch('/api/subjects/' . $subject1->getId(), [
+                'json' => [
+                    'name' => $subject2->getName(),
+                ],
+                'headers' => ['Content-Type' => 'application/merge-patch+json']
+            ])->assertStatus(422);
+    }
+
+    /** @test */
+    public function patch_to_update_teacher()
+    {
+        $subject = SubjectFactory::createOne();
+        $teacher = TeacherFactory::createOne();
+
+        $this->browser()
+            ->patch('/api/subjects/' . $subject->getId(), [
+                'json' => [
+                    'teacher' => '/api/teachers/' . $teacher->getId(),
+                ],
+                'headers' => ['Content-Type' => 'application/merge-patch+json'],
+            ])->assertStatus(200);
+
+        $this->assertSame($subject->getTeacher()->getId(), $teacher->getId());
+
+    }
+
+}
