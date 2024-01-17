6cd6847ac19870db8a9e0c2c11c93a2e0ef15dba
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-17 17:18:28 +0100

    added SubjectSchoolClassManagementApi api resource to add and remove school classes from the subject

---
+++ src/ApiResource/SubjectSchoolClassManagementApi.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\ApiResource;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\OpenApi\Model\Operation;
+use App\Dto\SubjectSchoolClassDto;
+use App\State\SubjectSchoolClassManagementStateProcessor;
+use App\State\SubjectSchoolClassManagementStateProvider;
+
+#[ApiResource(
+    shortName: 'Subject management',
+    operations: [
+        new Post(
+            uriTemplate: '/subjects/classes/add',
+            openapi: new Operation(
+                summary: 'Add a school class to the subject',
+                description: 'Add a school class to the subject',
+            ),
+        ),
+        new Delete(
+            uriTemplate: '/subjects/{subjectId}/classes/{schoolClassName}',
+            openapi: new Operation(
+                summary: 'Remove a school class from a subject',
+                description: 'Remove a school class from a subject',
+            ),
+            provider: SubjectSchoolClassManagementStateProvider::class,
+        ),
+    ],
+    input: SubjectSchoolClassDto::class,
+    processor: SubjectSchoolClassManagementStateProcessor::class,
+)]
+class SubjectSchoolClassManagementApi
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
