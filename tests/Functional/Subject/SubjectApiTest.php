1c66d0ce266774a1e5ff11c58cdd8adecc2df41c
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-17 17:19:48 +0100

    added tests

--- tests/Functional/Subject/SubjectApiTest.php
+++ tests/Functional/Subject/SubjectApiTest.php
@@ -2,12 +2,14 @@
 
 namespace App\Tests\Functional\Subject;
 
+use App\Factory\SchoolClassFactory;
 use App\Factory\SubjectFactory;
 use App\Factory\TeacherFactory;
 use App\Tests\Functional\Helper\ApiTestCase;
 use Zenstruck\Browser\Json;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
+use function PHPUnit\Framework\assertTrue;
 
 class SubjectApiTest extends ApiTestCase
 {
@@ -180,6 +182,73 @@ class SubjectApiTest extends ApiTestCase
             ])->assertStatus(200);
 
         $this->assertSame($subject->getTeacher()->getId(), $teacher->getId());
+
+    }
+
+
+    /** @test */
+    public function post_to_add_school_class()
+    {
+        $subject = SubjectFactory::createOne();
+        $schoolClass = SchoolClassFactory::createOne();
+
+        $this->browser()
+            ->post('/api/subjects/classes/add', [
+                'json' => [
+                    'subjectId' => $subject->getId(),
+                    'schoolClassName' => $schoolClass->getName(),
+                ]
+            ])->assertStatus(201);
+
+    }
+
+    /** @test */
+    public function post_to_add_school_class_with_not_existing_school_class()
+    {
+        $subject = SubjectFactory::createOne();
+
+        $this->browser()
+            ->post('/api/subjects/classes/add', [
+                'json' => [
+                    'subjectId' => $subject->getId(),
+                    'schoolClassName' => '7zz',
+                ]
+            ])->assertStatus(404);
+    }
+
+    /** @test */
+    public function post_to_add_school_class_not_existing_subject()
+    {
+        $schoolClass = SchoolClassFactory::createOne();
+
+        $this->browser()
+            ->post('/api/subjects/classes/add', [
+                'json' => [
+                    'subjectId' => 43,
+                    'schoolClassName' => $schoolClass->getName(),
+                ]
+            ])->assertStatus(404);
+    }
+
+    /** @test */
+    public function post_to_add_school_class_with_blank_data()
+    {
+        $this->browser()
+            ->post('/api/subjects/classes/add', [
+                'json' => [
+                ]
+            ])->assertStatus(422);
+    }
+
+    /** @test */
+    public function remove_school_class_from_subject()
+    {
+        $subject = SubjectFactory::createOne();
+        $schoolClassName = $subject->getSchoolClasses()->getValues()[0]->getName();
+
+        $this->browser()
+            ->delete('/api/subjects/'.$subject->getId().'/classes/'.$schoolClassName)
+            ->assertStatus(204);
 
     }
 
