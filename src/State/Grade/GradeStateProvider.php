5d672834c58912532bb6c4f9c765e82d6176be47
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-21 01:08:53 +0100

    update

--- src/State/Grade/GradeStateProvider.php
+++ src/State/Grade/GradeStateProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\State\Grade;
 
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Dto\StudentGradeDto;
@@ -20,9 +21,20 @@ class GradeStateProvider implements ProviderInterface
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
+        if ($operation instanceof Delete) {
+            $grade = $this->gradeRepository->findOneBy(['id' => $uriVariables['gradeId']]);
+
+            if (!$grade) {
+                throw new ResourceNotFoundException('Not found');
+            }
+
+            return $grade;
+        }
+
+
         $grades = $this->gradeRepository->findGradesByStudentAndSubjectName($uriVariables['studentId'], $uriVariables['subjectName']);
 
-        if(!$grades) {
+        if (!$grades) {
             throw new ResourceNotFoundException('Not found');
         }
 
