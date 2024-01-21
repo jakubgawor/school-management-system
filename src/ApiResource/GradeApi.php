6a2964e8d4ee27f4d345560e569e62ccb6337bba
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-21 01:08:01 +0100

    added get students grades and delete grade

--- src/ApiResource/GradeApi.php
+++ src/ApiResource/GradeApi.php
@@ -3,17 +3,31 @@
 namespace App\ApiResource;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Post;
 use App\Dto\AddGradeDto;
 use App\State\Grade\AddGradeStateProcessor;
+use App\State\Grade\DeleteGradeStateProcessor;
+use App\State\Grade\GradeStateProvider;
 
 #[ApiResource(
+    shortName: 'Grade',
     operations: [
         new Post(
             uriTemplate: '/grade/students/{studentId}',
             input: AddGradeDto::class,
             processor: AddGradeStateProcessor::class,
         ),
+        new Get(
+            uriTemplate: '/grades/students/{studentId}/{subjectName}',
+            provider: GradeStateProvider::class,
+        ),
+        new Delete(
+            uriTemplate: '/grade/{gradeId}',
+            provider: GradeStateProvider::class,
+            processor: DeleteGradeStateProcessor::class,
+        )
     ],
 )]
 class GradeApi
