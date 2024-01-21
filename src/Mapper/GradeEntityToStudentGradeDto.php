7704d36ff5b1082b9dc9b199b0780d08ec56cc89
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-21 01:08:32 +0100

    added issuedBy

--- src/Mapper/GradeEntityToStudentGradeDto.php
+++ src/Mapper/GradeEntityToStudentGradeDto.php
@@ -2,20 +2,27 @@
 
 namespace App\Mapper;
 
+use App\ApiResource\TeacherApi;
 use App\Dto\StudentGradeDto;
 use App\Entity\Grade;
 use Symfonycasts\MicroMapper\AsMapper;
 use Symfonycasts\MicroMapper\MapperInterface;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
 
 #[AsMapper(from: Grade::class, to: StudentGradeDto::class)]
 class GradeEntityToStudentGradeDto implements MapperInterface
 {
+    public function __construct(
+        private MicroMapperInterface $microMapper,
+    )
+    {
+    }
 
     public function load(object $from, string $toClass, array $context): object
     {
         $entity = $from;
 
-        $dto =  new StudentGradeDto();
+        $dto = new StudentGradeDto();
         $dto->id = $entity->getId();
 
         return $dto;
@@ -30,6 +37,7 @@ class GradeEntityToStudentGradeDto implements MapperInterface
 
         $dto->grade = $entity->getGrade();
         $dto->weight = $entity->getWeight();
+        $dto->issuedBy = $this->microMapper->map($entity->getTeacher(), TeacherApi::class);
 
         return $dto;
     }
