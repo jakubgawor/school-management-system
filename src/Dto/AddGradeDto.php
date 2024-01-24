ef06bb8aa8f2dc52e47ba3381900243166e1bfaa
Jakub Gawor <kuba.gawor1@gmail.com>
2024-01-24 22:48:33 +0100

    update

--- src/Dto/AddGradeDto.php
+++ src/Dto/AddGradeDto.php
@@ -2,13 +2,26 @@
 
 namespace App\Dto;
 
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\Type;
+
 class AddGradeDto
 {
     /** Subject name */
+    #[NotBlank]
+    #[NotNull]
     public ?string $subject = null;
 
+    #[NotBlank]
+    #[NotNull]
     public ?string $grade = null;
 
+    #[NotBlank]
+    #[NotNull]
+    #[Range(min: 1, max: 5)]
+    #[Type(type: ['integer', 'digit'])]
     public ?int $weight = null;
 
 }
